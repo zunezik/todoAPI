7ec51da0d542ec3bd79591adfcee6ca34802959e
Vitaliy Chuenko <zunezik@gmail.com>
2019-10-29 14:50:27 +0200

    implemented put and delete

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ function logRequest({method, url}, res, next) {
 app.use(express.json())
 app.use(logRequest)
 
-const tasks = [{name: 'Get tasks'}, {name: 'Create task'}]
+let tasks = [{id: 1, name: 'Get tasks'}, {id: 2, name: 'Create task'}]
 
 app.get('/tasks', (req, res) => res.json(tasks))
 
@@ -17,6 +17,27 @@ app.post('/tasks', (req, res) => {
     const task = req.body
     tasks.push(task)
     res.json(task)
+})
+
+app.put('/tasks', (req, res) => {
+    const newTask = req.body
+    const idx = tasks.findIndex((task) => task.id == newTask.id)
+
+    tasks = [
+        ...tasks.slice(0, idx),
+        newTask,
+        ...tasks.slice(idx + 1)
+    ]
+
+    res.json(newTask)
+})
+
+app.delete('/tasks', (req, res) => {
+    const taskToDelete = req.body
+    const idx = tasks.findIndex((task) => task.id == taskToDelete.id)
+    tasks.splice(idx, 1);
+
+    res.json(taskToDelete)
 })
 
 const port = 3000;
